d11a379e4f0828a643e14bf5adc9d9fe1cf52510
David Kunwar <dkunwar7111@gmail.com>
2022-11-21 20:46:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Form from './components/form'
+import Submitted from './components/submitted'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/user-form' element={<Form />} />
+        <Route path='/submitted' element={<Submitted />} />
+      </Routes>
     </div>
   );
 }
